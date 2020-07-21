c9b2531d219e68a2f4c01d7cbad9509d45f27528
manuelgeek <emashmagak@appslab.co.ke>
2020-07-21 14:24:53 +0300

    run mix format

--- kumbuka/lib/kumbuka/eraser.ex
+++ kumbuka/lib/kumbuka/eraser.ex
@@ -1,15 +1,18 @@
 defmodule Kumbuka.Eraser do
   defstruct [:text, :steps]
+
   def new(text, no_of_steps) do
     __struct__(text: text, steps: build_steps(text, no_of_steps))
   end
+
   defp build_steps(text, no_of_steps) do
     size = String.length(text)
-    (1..size)
-    |> Enum.shuffle
-    |> Enum.chunk_every(step_size(no_of_steps,size))
 
+    1..size
+    |> Enum.shuffle()
+    |> Enum.chunk_every(step_size(no_of_steps, size))
   end
+
   defp step_size(no_of_steps, size) do
     size
     |> Kernel./(no_of_steps)
